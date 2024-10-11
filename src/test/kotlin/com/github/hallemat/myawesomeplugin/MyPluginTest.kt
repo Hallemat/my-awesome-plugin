731baa4855aa770ad3ee7a691ec853905f1dd43b
GitHub Action <action@github.com>
2024-10-11 19:39:31 +0000

    Template cleanup

--- src/test/kotlin/com/github/hallemat/myawesomeplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/hallemat/myawesomeplugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hallemat.myawesomeplugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hallemat.myawesomeplugin.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
